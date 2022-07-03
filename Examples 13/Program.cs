f385eadcfcd8cb65a07f2168033266b948ae9188
Dima <novikov.dima@29gmail.com>
2022-07-03 09:46:40 +0300

    Сохранение 13 задачи

---
+++ Examples 13/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите число ");
+int N = Convert.ToInt32(Console.ReadLine());
+if (N > 9 & N <= 99)
+{
+   Console.WriteLine("Третьей цифры нет"); 
+}
+else if (N > 99 & N <= 999)
+{
+    int a = N%10;
+    Console.WriteLine(a); 
+}
+else if (N > 999 & N <= 9999)
+{
+    int b = N%100; int c = b/10;
+    Console.WriteLine(c);
+}
