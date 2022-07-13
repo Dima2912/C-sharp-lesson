149921292f149d8ff9bd4ae59036e3049b726e4a
Новиков Дмитрий Валериевич <novikovdv@teconpc.local>
2022-07-13 09:47:56 +0300

    Нахождение полиндрома

---
+++ Seminar3/Example19/Program.cs
@@ -0,0 +1,30 @@
+﻿int number = InputInt("Введите пятизначное число: ");
+
+int firstNumberDigit = number/10000;
+int secondNumberDigit = number%10000/1000;
+int thirdNumberDigit = number%1000/100;
+int fourthNumberDigit = number%100/10;
+int fifthNumberDigit = number%10;
+     
+if (number >= 10000 && number <= 99999)
+{
+    if(firstNumberDigit == fifthNumberDigit && secondNumberDigit == fourthNumberDigit)
+    {
+       Console.WriteLine("Число " + number + " является полиндромом"); 
+    }
+   else 
+   {
+    Console.WriteLine("Число " + number + " не является полиндромом"); 
+   }
+
+}
+else
+{
+    Console.WriteLine("Число не является пятизначным");
+}
+
+int InputInt(string output)   // Метод ввода числа с консоли
+{
+    Console.WriteLine(output);
+    return Convert.ToInt32(Console.ReadLine());
+}
