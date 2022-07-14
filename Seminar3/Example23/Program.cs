f573f2395b4575701e2ea45a2388d1daa7508d38
Новиков Дмитрий Валериевич <novikovdv@teconpc.local>
2022-07-14 18:40:57 +0300

    Сохранение задачи 23

---
+++ Seminar3/Example23/Program.cs
@@ -0,0 +1,13 @@
+﻿int userNumbers = InputInt("Введите число: ");
+
+for(int count = 1; count <= userNumbers; count++)
+{
+    int rez = count*count*count;
+    Console.WriteLine(rez);
+}
+
+int InputInt(string output)   // Метод ввода числа с консоли
+{
+    Console.WriteLine(output);
+    return Convert.ToInt32(Console.ReadLine());
+}
