68ea3c7f19064f8b774e7757fc4c471ce172f9a0
Dima <novikov.dima@29gmail.com>
2022-07-23 01:06:32 +0300

    Сохранение 41 и 43 задач

---
+++ Seminar6/Example43/Program.cs
@@ -0,0 +1,32 @@
+﻿
+double k1 = IntNumbers("Введите k1: ");
+double k2 = IntNumbers("Введите k2: ");
+double b1 = IntNumbers("Введите b1: ");
+double b2 = IntNumbers("Введите b2: ");
+double coordinatX = 0;
+double coordinatY = 0;
+
+if (k1 - k2 == 0)
+{
+    if (b1 - b2 == 0)
+    {
+        Console.WriteLine("Прямые совпадают");
+    }
+    else
+    {
+        Console.WriteLine("Прямые паралельны");
+    }
+}
+else 
+{
+    coordinatX = (b2 - b1) / (k1 - k2);
+    coordinatY = k1 * coordinatX + b1;
+    Console.WriteLine($"Координаты точки пересечения двух прямых ({coordinatX}, {coordinatY})");
+}
+
+
+double IntNumbers (string output)
+{
+    Console.Write(output);
+    return Convert.ToDouble(Console.ReadLine());
+}
---
+++ Seminar6/Examples41/Program.cs
@@ -0,0 +1,101 @@
+﻿string userNumericalSequence = GetUserAnswer("Введите последовательность чисел разделенных запятой: ");
+
+int[] userArray = SetNumbers(userNumericalSequence);
+
+Numbers(userArray);
+
+int count = 0;
+for (int i = 0; i < userArray.Length; i++)
+{
+    if (userArray[i] >= 0)
+    {
+        count++;
+    }
+}
+Console.WriteLine($"Количество числе больше 0 равно: {count}");
+
+
+string GetUserAnswer (string text)
+{
+    Console.WriteLine(text);
+    string userNumber = Console.ReadLine() ?? "";
+    return userNumber;
+}
+
+
+
+int[] GetArray (int[] array, string userString)
+{
+    string result = String.Empty;
+    int startIndex = 0;
+    for (int index = 0; index < userString.Length; index++)
+    {   
+        if (userString[index] != ' ' && userString[index] != ',')
+        {
+            result += userString[index];
+        }
+        if ((userString[index] == ' ' || userString[index] == ',') && result != String.Empty)
+        {
+            array[startIndex] = Convert.ToInt32(result);
+            result = string.Empty;
+            startIndex++;
+        }
+        if (index == userString.Length - 1 && result != String.Empty)
+        {
+            array[startIndex] = Convert.ToInt32(result);
+        }
+    }
+    return array;
+}
+
+
+int GetNumberOfCommas (string stringWithNumbers)
+{
+    int space = 0;
+
+    for (int count = 0; count < stringWithNumbers.Length; count++)
+    {
+        if (stringWithNumbers[count] == ',' && count != stringWithNumbers.Length - 1)
+        {
+            space += 1;
+        }
+    }
+    return space;
+}
+
+
+int[] SetNumbers (string userString)
+{   
+    int[] array = new int [GetNumberOfCommas(userString) + 1];
+    array = GetArray(array, userString);
+    return array;
+}
+
+
+void Numbers (int[] array)
+{
+    string result = String.Empty; // Пустая строка
+    for (int count = 0; count < array.Length; count++)
+    {
+        if (count == 0 && count == array.Length - 1)
+        {
+            result += $"[{array[count]}]";
+        }
+        else if (count == 0)
+        {
+            result += $"[{array[count]}, ";
+        }
+        else if (count == array.Length - 1)
+        {
+            result += $"{array[count]}]";
+        }
+        else
+        {
+            result += $"{array[count]}, ";
+        }
+    }
+    Console.WriteLine();
+    Console.WriteLine(result);
+    Console.WriteLine();
+}
+
