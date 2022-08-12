6642d81eb436c6459815c102aaa382d707048e1d
Dima <novikov.dima@29gmail.com>
2022-08-13 01:06:38 +0300

    Сохранение задач 9 семинара

---
+++ Seminar9/Example 68/Program.cs
@@ -0,0 +1,22 @@
+﻿int GetNumber (string output)
+{
+    Console.Write(output);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int AckermannFunction (int m, int n)
+{
+    if (m == 0)
+        return n + 1;
+    else if (m > 0 && n == 0)
+        return AckermannFunction(m - 1, 1);
+    else
+        return AckermannFunction(m - 1, AckermannFunction(m, n-1)); 
+}
+
+int number1 = GetNumber("Введите число №1: "),
+    number2 = GetNumber("Введите число №2: ");
+
+Console.Write(AckermannFunction(number1, number2));
+Console.WriteLine();
+
